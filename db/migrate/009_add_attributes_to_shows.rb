7e86582be88d76e011e7f6447cd3dd84450043f8
Henna Vahatiitto <hnnamv@gmail.com>
2020-03-11 13:59:37 +0000

    Automatically backed up by Learn

--- db/migrate/009_add_attributes_to_shows.rb
+++ db/migrate/009_add_attributes_to_shows.rb
@@ -3,5 +3,6 @@ class AddAttributesToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
+    add_column :shows, :season, :string
   end
 end
